cce6fc9bfe98c1e2221cd6f25ef2e106b52d368c
Ayshan Safarova <aysensafarova@gmail.com>
2019-01-16 15:32:59 +0400

    lesson day 2

--- src/index.js
+++ src/index.js
@@ -14,29 +14,65 @@
 // Learn more about service workers: http://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
 
-var style = {
-	backgroundColor: 'blue',
-	color: 'white'
-}
-
+//START
 // const title = React.createElement(
 // 	'h2', //element
 // 	{id: 'title', className: 'header', style: style}, 
 // 	'Congrats!!!' //text
 // )
-
 // ReactDOM.render(
 // 	title, //which we will run
 // 	document.getElementById('root')
 // )
 
-ReactDOM.render(
-	<div style={style} id="hohohohoh">
-		<h1>Ho Ho Ho</h1>
-		<h5>I am coming, here!!!</h5>
-	</div>,
+//Lesson Day 1
+var style = {
+	backgroundColor: 'blue',
+	color: 'white'
+}
+// ReactDOM.render(
+// 	<div style={style} id="hohohohoh">
+// 		<h1>Ho Ho Ho</h1>
+// 		<h5>I am coming, here!!!</h5>
+// 	</div>,
+// 	document.getElementById('root')
+// )
+
+//Lesson Day 2
+let bookData = {
+	namee: "Harry Potter",
+	year: 2008,
+	author: "J.K.Rowling"
+}
+class Message extends Component{
+	getYears = (lastYear) => {
+		return lastYear - bookData.year
+	}
+	render(){
+		const {clr, date, txt} = this.props
+		return(
+			<React.Fragment>
+			<section>
+				<h1 style = {{color: clr}}>{txt}</h1>
+				<h4 style = {style} >Today is {date} </h4>
+			</section>
+			<section>
+				<ul>
+					<li>Book Name: {this.props.namee}</li>
+					<li>Book Year: {this.props.year}</li>
+					<li>Book Author: {this.props.author}</li>
+					<li>Passed Years: {this.getYears(2019)}</li>
+				</ul>
+			</section>
+			</React.Fragment>
+		)
+	}
+}
+render(
+	<Message txt = "Lesson Day 2" clr = "blue" date = "16 January, 2019" 
+	namee = {bookData.namee} year = {bookData.year} author = {bookData.author}/>,
 	document.getElementById('root')
 )
