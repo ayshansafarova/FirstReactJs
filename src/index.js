5fab57e6566ee7a4cce2a16564e094a49dd94ac6
Ayshan Safarova <aysensafarova@gmail.com>
2019-02-11 16:52:16 +0400

    learning

--- src/index.js
+++ src/index.js
@@ -42,11 +42,11 @@ import ReactDOM, {render} from 'react-dom';
 // // )
 
 // //Lesson Day 2
-let bookData = {
-	namee: "Harry Potter",
-	year: 2008,
-	author: "J.K.Rowling"
-}
+// let bookData = {
+// 	namee: "Harry Potter",
+// 	year: 2008,
+// 	author: "J.K.Rowling"
+// }
 // class Message extends Component{
 // 	getYears = (lastYear) => {
 // 		return lastYear - bookData.year
@@ -78,6 +78,82 @@ let bookData = {
 // )
 
 //Lesson Day 3
+// const Book = (props) => {
+// 	return(
+// 		<section>
+// 			<h2>{props.namee}</h2>
+// 			<h4>{props.year}</h4>
+// 			<h4>{props.author}</h4>
+// 		</section>
+// 	)
+// }
+// render(
+// 	<Book namee = {bookData.namee} year = {bookData.year} author = {bookData.author} />,
+// 	document.getElementById('root')
+// )
+
+//Lesson Day 4 
+var style = {
+	color: 'red'
+}
+let booksList = [
+	{
+		namee: "Harry Potter",
+		year: 2008,
+		author: "J.K.Rowling"
+	},
+	{
+		namee: "Twilight",
+		year: 2006,
+		author: "Stephany R."
+	},	{
+		namee: "Alchemist",
+		year: 2012,
+		author: "Paulo Coelho"
+	},	{
+		namee: "Carrie",
+		year: 1995,
+		author: "Steven King"
+	},	{
+		namee: "Pride and Prejudice",
+		year: 1988,
+		author: "Jane Austen"
+	}
+]
+// const Book = (props) => {
+// 	return(
+// 		<section>
+// 			<h2>{props.namee}</h2>
+// 			<h4>{props.year}</h4>
+// 			<h4>{props.author}</h4>
+// 		</section>
+// 	)
+// }
+// const Library = ({books}) => {
+// 	return(
+// 		<React.Fragment>
+// 		<h1 style={style}>
+// 			Welcome to the Library :)
+// 		</h1>
+// 		<div>
+// 			{
+// 				books.map(
+// 					(bookData, i) => 
+// 						<Book key = {i} namee = {bookData.namee} year = {bookData.year} author = {bookData.author}/>
+// 				)
+// 			}
+// 		</div>
+// 		</React.Fragment>
+// 	)
+// }
+
+
+// render(
+// 	<Library books = {booksList} />,
+// 	document.getElementById('root')
+// )
+
+//Lesson Day 5
 const Book = (props) => {
 	return(
 		<section>
@@ -87,7 +163,34 @@ const Book = (props) => {
 		</section>
 	)
 }
+class Library extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			open: true
+		}
+	}
+	render(){
+		const {books} = this.props
+		return(
+			<React.Fragment>
+			<h1 style={style}>
+				The Library is {this.state.open ? 'open' : 'closed'}
+			</h1>
+			<div>
+				{
+					books.map(
+						(bookData, i) => 
+							<Book key = {i} namee = {bookData.namee} year = {bookData.year} author = {bookData.author}/>
+					)
+				}
+			</div>
+			</React.Fragment>
+		)
+	}
+}
+
 render(
-	<Book namee = {bookData.namee} year = {bookData.year} author = {bookData.author} />,
+	<Library books = {booksList} />,
 	document.getElementById('root')
 )
